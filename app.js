5c8bf5e7050230db06f9ac8549d6839e9fc10e2d
unknown <edmundpunch5@gmail.com>
2023-05-15 14:00:14 -0400

    Fixed dbUrl

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-// const dbUrl = process.env.DB_URL Production database
-const dbUrl = 'mongodb://localhost:27017/yelp-camp' // Development database
+const dbUrl = process.env.DB_URL // Production database
+//const dbUrl = 'mongodb://localhost:27017/yelp-camp' // Development database
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
